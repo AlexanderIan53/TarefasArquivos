60a87665f14d4b1413c0204805aca8334dd744c2
Ian McIntosh <61014227+Crian53@users.noreply.github.com>
2020-10-31 10:46:34 -0300

    Update busca.c

--- busca.c
+++ busca.c
@@ -4,13 +4,13 @@
 #include<ctype.h>
 typedef struct _Endereco Endereco;
 struct _Endereco{
-	char logradouro[72];
-	char bairro[72];
-	char cidade[72];
-	char uf[72];
-	char sigla[2];
-	char cep[8];
-	char lixo[2];
+char logradouro[72];
+char bairro[72];
+char cidade[72];
+char uf[72];
+char sigla[2];
+char cep[8];
+char lixo[2];
 };
 void main(){
 FILE *f;
